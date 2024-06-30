3053128938c0c6db7cf6d9ecea6f8d1f04b25c6c
andrey4281 <Andrey4281@yandex.ru>
2024-06-30 15:49:13 +0500

    Homework 16

--- hw15/procedure.sql
+++ hw15/procedure.sql
@@ -13,67 +13,41 @@ CREATE PROCEDURE getGoods(search varchar(256),
                           limitValue int unsigned,
                           offsetValue int unsigned)
 BEGIN
-    SET @baseQuery = 'SELECT p.name, m.name, pc.name, pi.amount, pi.price, pi.delivery_date
+    SET @baseQuery = 'SELECT
+    p.name,
+    m.name,
+    pc.name,
+    pi.amount,
+    pi.price,
+    pi.delivery_date
 FROM otus.product p
          INNER JOIN otus.manufacturer m ON p.manufacturer_fk = m.id
          INNER JOIN otus.product_category_ref pcr ON pcr.product_fk = p.id
          INNER JOIN otus.product_category pc ON pc.id = pcr.product_category_fk
-         INNER JOIN otus.product_item pi ON pi.product_fk = p.id';
+         INNER JOIN otus.product_item pi ON pi.product_fk = p.id WHERE 1=1';
     SET @filters = '';
-    IF (search IS NOT NULL
-        OR categoryId IS NOT NULL
-        OR manufacturerId IS NOT NULL
-        OR lowerBorderPrice IS NOT NULL
-        OR rightBorderPrice IS NOT NULL
-        OR leftBorderDeliveryDate IS NOT NULL
-        OR rightBorderDeliveryDate IS NOT NULL) THEN
-        SET @filters = CONCAT(@filters, ' WHERE ');
-        SET @hasOtherFilter = false;
-        IF (search IS NOT NULL) THEN
-            SET @hasOtherFilter = true;
-            SET @filters = CONCAT(@filters, REPLACE('MATCH(description) AGAINST (''$value'')', '$value', search));
-        END IF;
-        IF (categoryId IS NOT NULL) THEN
-            IF (@hasOtherFilter = true) THEN
-                SET @filters = CONCAT(@filters, ' AND ');
-            END IF;
-            SET @filters = CONCAT(@filters, REPLACE(' pc.id = $value', '$value', categoryId));
-            SET @hasOtherFilter = true;
-        END IF;
-        IF (manufacturerId IS NOT NULL) THEN
-            IF (@hasOtherFilter = true) THEN
-                SET @filters = CONCAT(@filters, ' AND ');
-            END IF;
-            SET @filters = CONCAT(@filters, REPLACE(' m.id = $value', '$value', manufacturerId));
-            SET @hasOtherFilter = true;
-        END IF;
-        IF (lowerBorderPrice IS NOT NULL) THEN
-            IF (@hasOtherFilter = true) THEN
-                SET @filters = CONCAT(@filters, ' AND ');
-            END IF;
-            SET @filters = CONCAT(@filters, REPLACE(' pi.price > $value', '$value', lowerBorderPrice));
-            SET @hasOtherFilter = true;
-        END IF;
-        IF (rightBorderPrice IS NOT NULL) THEN
-            IF (@hasOtherFilter = true) THEN
-                SET @filters = CONCAT(@filters, ' AND ');
-            END IF;
-            SET @filters = CONCAT(@filters, REPLACE(' pi.price < $value', '$value', rightBorderPrice));
-            SET @hasOtherFilter = true;
-        END IF;
-        IF (leftBorderDeliveryDate IS NOT NULL) THEN
-            IF (@hasOtherFilter = true) THEN
-                SET @filters = CONCAT(@filters, ' AND ');
-            END IF;
-            SET @filters = CONCAT(@filters, REPLACE(' pi.delivery_date >= ''$value''', '$value', leftBorderDeliveryDate));
-            SET @hasOtherFilter = true;
-        END IF;
-        IF (rightBorderDeliveryDate IS NOT NULL) THEN
-            IF (@hasOtherFilter = true) THEN
-                SET @filters = CONCAT(@filters, ' AND ');
-            END IF;
-            SET @filters = CONCAT(@filters, REPLACE(' pi.delivery_date <= ''$value''', '$value', rightBorderDeliveryDate));
-        END IF;
+    IF (search IS NOT NULL) THEN
+        SET @filters = CONCAT(@filters, REPLACE(' AND MATCH(description) AGAINST (''$value'')', '$value', search));
+    END IF;
+    IF (categoryId IS NOT NULL) THEN
+        SET @filters = CONCAT(@filters, REPLACE(' AND pc.id = $value', '$value', categoryId));
+    END IF;
+    IF (manufacturerId IS NOT NULL) THEN
+        SET @filters = CONCAT(@filters, REPLACE(' AND m.id = $value', '$value', manufacturerId));
+    END IF;
+    IF (lowerBorderPrice IS NOT NULL) THEN
+        SET @filters = CONCAT(@filters, REPLACE(' AND pi.price > $value', '$value', lowerBorderPrice));
+    END IF;
+    IF (rightBorderPrice IS NOT NULL) THEN
+        SET @filters = CONCAT(@filters, REPLACE(' AND pi.price < $value', '$value', rightBorderPrice));
+    END IF;
+    IF (leftBorderDeliveryDate IS NOT NULL) THEN
+        SET @filters =
+                CONCAT(@filters, REPLACE(' AND pi.delivery_date >= ''$value''', '$value', leftBorderDeliveryDate));
+    END IF;
+    IF (rightBorderDeliveryDate IS NOT NULL) THEN
+        SET @filters =
+                CONCAT(@filters, REPLACE(' AND pi.delivery_date <= ''$value''', '$value', rightBorderDeliveryDate));
     END IF;
     SET @sort = '';
     IF (sortColumn IS NOT NULL) THEN
@@ -98,6 +72,9 @@ DELIMITER ;
 CREATE USER 'client'@'localhost' IDENTIFIED BY 'client';
 GRANT EXECUTE ON PROCEDURE otus.getGoods TO 'client'@'localhost';
 
+CALL otus.getGoods('Apple', 1, 1, 10.0, 130000.00, '2024-06-06', '2024-06-09', 'pi.price', false, 3, 1);
+CALL getGoods(null, 1, null, 10.0, 130000.00, null, null, 'pi.price', false, 3, 1);
+
 -- Sales report for a certain period with different levels of grouping (by product, by category, by manufacturer):
 DELIMITER $$
 USE otus $$
@@ -105,12 +82,16 @@ CREATE PROCEDURE getOrdersReport(leftBorderDeliveryDate date,
                                  rightBorderDeliveryDate date,
                                  groupingClause ENUM ('product', 'manufacturer', 'category'))
 BEGIN
-    SET @baseQuery = 'SELECT ${group.column}, sum(pi.total_cost) as totalCost, sum(pi.amount) as totalAmount FROM purchase p
-                                                                                        INNER JOIN otus.purchase_item pi on p.id = pi.purchase_id
-                                                                                        INNER JOIN otus.product pro on pi.product_id = pro.id
-                                                                                        INNER JOIN otus.manufacturer m ON pro.manufacturer_fk = m.id
-                                                                                        INNER JOIN otus.product_category_ref pcr ON pcr.product_fk = p.id
-                                                                                        INNER JOIN otus.product_category pc ON pc.id = pcr.product_category_fk
+    SET @baseQuery = 'SELECT
+    ${group.column},
+    sum(pi.total_cost) as totalCost,
+    sum(pi.amount) as totalAmount
+FROM purchase p
+     INNER JOIN otus.purchase_item pi on p.id = pi.purchase_id
+     INNER JOIN otus.product pro on pi.product_id = pro.id
+     INNER JOIN otus.manufacturer m ON pro.manufacturer_fk = m.id
+     INNER JOIN otus.product_category_ref pcr ON pcr.product_fk = p.id
+     INNER JOIN otus.product_category pc ON pc.id = pcr.product_category_fk
 WHERE pi.delivery_date BETWEEN  ''${leftBorderDeliveryDate}'' AND ''${rightBorderDeliveryDate}''
 GROUP BY ${group.column}';
     IF (leftBorderDeliveryDate IS NULL) THEN
