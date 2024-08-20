6e1ac1f3cec45d18999bb7a6e48f00b8db6bc31b
andrey4281 <Andrey4281@yandex.ru>
2024-08-20 10:11:09 +0500

    project

--- project/queries.sql
+++ project/queries.sql
@@ -1,7 +1,5 @@
--- найти товары в своем городе(стране) по полнотекстовому поиску, производителю, категории
-
 -- в своем городе
-SELECT p.name, m.name, pc.name, pi.amount, pi.price
+SELECT p.id, p.name, m.name, pc.name, pi.amount, pi.price, pi.supplier_fk
 FROM otus.customer cu
          INNER JOIN otus.customer_contact_data ccd ON cu.id = ccd.customer_fk
          INNER JOIN otus.warehouse_contact_data wcd ON ccd.city_fk = wcd.city_fk
@@ -14,7 +12,7 @@ FROM otus.customer cu
          INNER JOIN otus.product_category pc ON (pc.id = pcr.product_category_fk);
 
 -- в своей стране
-SELECT p.name, m.name, pc.name, pi.amount, pi.price
+SELECT p.id, p.name, m.name, pc.name, pi.amount, pi.price, pi.supplier_fk
 FROM otus.customer cu
          INNER JOIN otus.customer_contact_data ccd ON cu.id = ccd.customer_fk
          INNER JOIN otus.warehouse_contact_data wcd ON ccd.country_fk = wcd.country_fk
@@ -25,3 +23,5 @@ FROM otus.customer cu
          INNER JOIN otus.unit u ON (u.id = p.unit_fk)
          INNER JOIN otus.product_category_ref pcr ON (pcr.product_fk = p.id)
          INNER JOIN otus.product_category pc ON (pc.id = pcr.product_category_fk);
+
+-- добавить тип доставки, если не выбирается свой город и страна
