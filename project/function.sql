190422a51cd99460be1ef4dc8e9f95a27f0ad694
andrey4281 <Andrey4281@yandex.ru>
2024-08-25 18:02:08 +0500

    project

--- project/function.sql
+++ project/function.sql
@@ -1,1 +1,44 @@
--- See hw15/procedure.sql
+CREATE OR REPLACE FUNCTION getOrdersReport(leftBorderDeliveryDate date,
+                                           rightBorderDeliveryDate date,
+                                           groupingClause otus.reportGroupingClause DEFAULT 'product')
+    RETURNS TABLE
+            (
+                group_column varchar,
+                totalCost    decimal(19, 4),
+                totalAmount  decimal(10, 2)
+            )
+AS
+$$
+DECLARE baseQuery varchar(2024) = 'SELECT
+    ${group.column} as group_column,
+    sum(oi.total_cost) as totalCost,
+    sum(oi.amount) as totalAmount
+FROM otus.order_item oi
+         INNER JOIN otus.product_item pi on oi.product_item_id = pi.id
+         INNER JOIN otus.product p ON (pi.product_fk = p.id)
+         INNER JOIN otus.manufacturer m ON p.manufacturer_fk = m.id
+         INNER JOIN otus.product_category_ref pcr ON pcr.product_fk = p.id
+         INNER JOIN otus.product_category pc ON pc.id = pcr.product_category_fk
+WHERE oi.delivery_date BETWEEN  ''${leftBorderDeliveryDate}'' AND ''${rightBorderDeliveryDate}''
+GROUP BY ${group.column}';
+BEGIN
+    IF (leftBorderDeliveryDate IS NULL) THEN
+        RAISE EXCEPTION 'NULL is not allowed for leftBorderDeliveryDate.';
+END IF;
+IF (rightBorderDeliveryDate IS NULL) THEN
+        RAISE EXCEPTION 'NULL is not allowed for rightBorderDeliveryDate.';
+END IF;
+baseQuery = replace(baseQuery, '${leftBorderDeliveryDate}', leftBorderDeliveryDate::varchar);
+baseQuery = replace(baseQuery, '${rightBorderDeliveryDate}', rightBorderDeliveryDate::varchar);
+IF (groupingClause = 'product') THEN
+        baseQuery = replace(baseQuery, '${group.column}', 'p.name');
+ELSEIF (groupingClause = 'manufacturer') THEN
+        baseQuery = replace(baseQuery, '${group.column}', 'm.name');
+ELSEIF (groupingClause = 'category') THEN
+        baseQuery = replace(baseQuery, '${group.column}', 'pc.name');
+END IF;
+RETURN QUERY EXECUTE baseQuery;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT group_column, totalCost, totalAmount FROM getOrdersReport('2024-01-01', '2028-01-01', 'product');
